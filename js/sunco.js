8438a0ade270c572c343f18b0b4a239dbc26a8d6
rom1henry <romain.ah@gmail.com>
2023-01-30 21:17:40 +0100

    Create sunco.js

---
+++ js/sunco.js
@@ -0,0 +1,9 @@
+  // Sunco Init
+  Smooch.init({ integrationId: '63cae66df56d2f0109c44af3' }).then(
+    function() {
+        // Your code after init is complete
+    },
+    function(err) {
+        // Something went wrong during initialization
+    }
+);
