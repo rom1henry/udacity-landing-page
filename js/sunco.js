f615ebb3d7503b526cdfbe67da07bcd201d9151c
rom1henry <romain.ah@gmail.com>
2023-11-17 14:11:03 +0100

    Update sunco.js - create conversation

--- js/sunco.js
+++ js/sunco.js
@@ -1,9 +1,37 @@
-  // Sunco Init
-  Smooch.init({ integrationId: '655752c4d99f34ed48c04bd7' }).then(
-    function() {
-        // Your code after init is complete
-    },
-    function(err) {
-        // Something went wrong during initialization
-    }
-);
+// Sunco Init
+Smooch.init({
+	integrationId: '655752c4d99f34ed48c04bd7',
+}).then(() => {
+	var iframe = document.getElementById('web-messenger-container');
+	var iframedocument = iframe.contentDocument || iframe.contentWindow.document;
+	iframedocument.addEventListener(
+		'click',
+		function (evt) {
+			initializeButton(iframedocument);
+		},
+		false
+	);
+});
+
+Smooch.on('widget:opened', function () {
+	if (!Smooch.getUser() || Smooch.getConversations().length === 0) {
+		Smooch.createConversation({
+			displayName: 'First Convo',
+			description: 'How can we help you?',
+		});
+	}
+});
+
+function initializeButton(iframedocument) {
+	var conversationFooter = iframedocument.getElementsByClassName('conversation-group-footer')[0];
+	if (conversationFooter) {
+		conversationFooter.innerHTML = '<button onclick=parent.createConversation()>New Conversation</button>';
+	}
+}
+function createConversation() {
+	if (Smooch.isOpened()) {
+		Smooch.createConversation().then((conversation) => {
+			Smooch.loadConversation(conversation.id);
+		});
+	}
+}
