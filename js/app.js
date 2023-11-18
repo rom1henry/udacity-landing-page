2faf8d739c489ea2ed556176858d0b350d0f139a
Romain Henry <romain.henry@zendesk.com>
2023-11-18 17:08:42 +0100

    sunco login/logout

--- js/app.js
+++ js/app.js
@@ -12,154 +12,213 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 // only execute script in <head> when DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-/**
- * Define Global Variables
-*/
+document.addEventListener(
+	'DOMContentLoaded',
+	() => {
+		/**
+		 * Define Global Variables
+		 */
 
-  const sections = document.querySelectorAll('section');
-  const pageHeader = document.querySelector('.page__header');
-  const nav = document.querySelector('#navbar__list');
-  const scrollBtn = document.querySelector('.scroll-top_btn');
-  let lastScrollPos = window.pageYOffset;
-  let scrollTimer = null;
-  let isOnNav = Boolean;
+		const sections = document.querySelectorAll('section');
+		const pageHeader = document.querySelector('.page__header');
+		const nav = document.querySelector('#navbar__list');
+		const scrollBtn = document.querySelector('.scroll-top_btn');
+		let lastScrollPos = window.pageYOffset;
+		let scrollTimer = null;
+		let isOnNav = Boolean;
 
-  /**
- * End Global Variables
- * Start Helper Functions
- *
-*/
+		const userData = {
+			external_id: 'm-scott',
+			name: 'Michael Scott',
+			email: 'michael-scott@example.com',
+		};
 
-  // Build the nav menu
-  const buildMenu = () => {
-    // Use documentFragment for performance during iteration
-    const tempNav = document.createDocumentFragment();
+		/**
+		 * End Global Variables
+		 * Start Helper Functions
+		 *
+		 */
 
-    for (const section of sections) {
-      const navItemLink = document.createElement('a');
+		// Build the nav menu
+		const buildMenu = () => {
+			// Use documentFragment for performance during iteration
+			const tempNav = document.createDocumentFragment();
 
-      // Navigation to anchors is done by scrollTo, so we use a custom attribute instead of href
-      navItemLink.dataset.nav = section.getAttribute('id');
-      navItemLink.innerText = section.dataset.nav;
-      navItemLink.classList.add('menu__link');
+			for (const section of sections) {
+				const navItemLink = document.createElement('a');
 
-      const navItem = document.createElement('li');
-      navItem.appendChild(navItemLink);
+				// Navigation to anchors is done by scrollTo, so we use a custom attribute instead of href
+				navItemLink.dataset.nav = section.getAttribute('id');
+				navItemLink.innerText = section.dataset.nav;
+				navItemLink.classList.add('menu__link');
 
-      tempNav.appendChild(navItem);
-    }
-    const navLoginLink = document.createElement('a');
-    navLoginLink.dataset.nav = 'login';
-    navLoginLink.innerText = 'Login';
-    navLoginLink.classList.add('menu__link');
-    
-    const navLogin = document.createElement('li');
-    navLogin.appendChild(navLoginLink);
+				const navItem = document.createElement('li');
+				navItem.appendChild(navItemLink);
 
-    tempNav.appendChild(navLogin);
-    
-    nav.appendChild(tempNav);
-  };
+				tempNav.appendChild(navItem);
+			}
+			const navLoginLink = document.createElement('a');
+			navLoginLink.dataset.nav = 'login';
+			navLoginLink.innerText = 'Login';
+			navLoginLink.classList.add('menu__link');
 
-  // Add class 'active' to section when near top of viewport, and anchor in the nav menu
-  const makeActive = () => {
-    for (const section of sections) {
-      const box = section.getBoundingClientRect();
-      if (box.top <= 150 && box.bottom >= 150) {
-        // Apply active state on the current section and the corresponding nav link.
-        section.classList.add('your-active-class');
-        nav.querySelector(`[data-nav="${section.id}"]`).classList.add('menu__link--active');
-      } else {
-        // Remove active state from other sections and other nav links
-        section.classList.remove('your-active-class');
-        nav.querySelector(`[data-nav="${section.id}"]`).classList.remove('menu__link--active');
-      }
-    }
-  };
+			const navLogin = document.createElement('li');
+			navLogin.appendChild(navLoginLink);
 
-  // Scroll smoothly to element
-  const scrollToElement = (element) => {
-    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-  };
+			tempNav.appendChild(navLogin);
 
-  // Toggle scroll-top btn visibility
-  const toggleBtn = () => {
-    if (window.scrollY > 500) {
-      scrollBtn.classList.remove('scroll-top_btn--hidden');
-    } else {
-      scrollBtn.classList.add('scroll-top_btn--hidden');
-    }
-  };
+			nav.appendChild(tempNav);
+		};
 
-  // Move header upwards by its height (invoked in next 2 functions)
-  const moveHeaderUp = () => {
-    pageHeader.style.top = `-${pageHeader.getBoundingClientRect().height}px`;
-  };
+		// Add class 'active' to section when near top of viewport, and anchor in the nav menu
+		const makeActive = () => {
+			for (const section of sections) {
+				const box = section.getBoundingClientRect();
+				if (box.top <= 150 && box.bottom >= 150) {
+					// Apply active state on the current section and the corresponding nav link.
+					section.classList.add('your-active-class');
+					nav.querySelector(`[data-nav="${section.id}"]`).classList.add('menu__link--active');
+				} else {
+					// Remove active state from other sections and other nav links
+					section.classList.remove('your-active-class');
+					nav.querySelector(`[data-nav="${section.id}"]`).classList.remove('menu__link--active');
+				}
+			}
+		};
 
-  // Toggle nav based on scroll direction
-  const toggleNavOnScroll = () => {
-    const currentScrollPos = window.pageYOffset;
-    if (lastScrollPos > currentScrollPos) {
-      pageHeader.style.top = '0';
-    } else {
-      moveHeaderUp();
-    }
-    lastScrollPos = currentScrollPos;
-  };
+		// Scroll smoothly to element
+		const scrollToElement = (element) => {
+			element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		};
 
-  // Toggle nav based on time since scroll
-  const toggleNavOnWait = () => {
-    if (scrollTimer) {
-      clearTimeout(scrollTimer);
-    }
-    // Check that mouse isn't on nav
-    if (!isOnNav) {
-      scrollTimer = setTimeout(() => {
-        moveHeaderUp();
-      }, 2500);
-    }
-  };
+		// Toggle scroll-top btn visibility
+		const toggleBtn = () => {
+			if (window.scrollY > 500) {
+				scrollBtn.classList.remove('scroll-top_btn--hidden');
+			} else {
+				scrollBtn.classList.add('scroll-top_btn--hidden');
+			}
+		};
 
-  /**
- * End Helper Functions
- * Begin Main Functions
- * */
+		// Move header upwards by its height (invoked in next 2 functions)
+		const moveHeaderUp = () => {
+			pageHeader.style.top = `-${pageHeader.getBoundingClientRect().height}px`;
+		};
 
-  buildMenu();
+		// Toggle nav based on scroll direction
+		const toggleNavOnScroll = () => {
+			const currentScrollPos = window.pageYOffset;
+			if (lastScrollPos > currentScrollPos) {
+				pageHeader.style.top = '0';
+			} else {
+				moveHeaderUp();
+			}
+			lastScrollPos = currentScrollPos;
+		};
 
-  // Invoke makeActive(), toggleBtn(), and toggleNavOnScroll/OnWait() when document is scrolled
-  document.addEventListener('scroll', () => {
-    makeActive();
-    toggleBtn();
-    toggleNavOnScroll();
-    toggleNavOnWait();
-  });
+		// Toggle nav based on time since scroll
+		const toggleNavOnWait = () => {
+			if (scrollTimer) {
+				clearTimeout(scrollTimer);
+			}
+			// Check that mouse isn't on nav
+			if (!isOnNav) {
+				scrollTimer = setTimeout(() => {
+					moveHeaderUp();
+				}, 2500);
+			}
+		};
 
-  // Invoke toggleNavOnWait based on mouse event on nav
-  nav.addEventListener('mouseenter', () => { isOnNav = true; toggleNavOnWait(); });
-  nav.addEventListener('mouseout', () => { isOnNav = false; toggleNavOnWait(); });
+		/**
+		 * End Helper Functions
+		 * Begin Main Functions
+		 * */
 
-  // Invoke scrollToElement when anchor in nav is clicked and send to the corresponding section
-  nav.addEventListener('click', (e) => {
-    const { target } = e;
-    // We delegate the event to a tags only inside the nav
-    if (target.tagName !== 'A') return;
-    // Get the section that matches the anchor's custom attribute
-    const section = document.querySelector(`#${target.dataset.nav}`);
-    scrollToElement(section);
-  });
+		buildMenu();
 
-  // Invoke scrollToElement when scrollBtn is clicked and send to the top of the HTML doc
-  scrollBtn.addEventListener('click', () => {
-    scrollToElement(document.documentElement);
-  });
-}, false);
+		// Invoke makeActive(), toggleBtn(), and toggleNavOnScroll/OnWait() when document is scrolled
+		document.addEventListener('scroll', () => {
+			makeActive();
+			toggleBtn();
+			toggleNavOnScroll();
+			toggleNavOnWait();
+		});
+
+		// Invoke toggleNavOnWait based on mouse event on nav
+		nav.addEventListener('mouseenter', () => {
+			isOnNav = true;
+			toggleNavOnWait();
+		});
+		nav.addEventListener('mouseout', () => {
+			isOnNav = false;
+			toggleNavOnWait();
+		});
+
+		// Invoke scrollToElement when anchor in nav is clicked and send to the corresponding section
+		nav.addEventListener('click', async (e) => {
+			const { target } = e;
+			// We delegate the event to a tags only inside the nav
+			if (target.tagName !== 'A') return;
+
+			//Call sunco login for login/logout nav element
+			if (target.getAttribute('data-nav') === 'login') {
+				await loginUser(userData).then(function () {
+					target.dataset.nav = 'logout';
+					target.innerText = 'Logout';
+				});
+				return;
+			}
+
+			if (target.getAttribute('data-nav') === 'logout') {
+				await logoutUser().then(function () {
+					target.dataset.nav = 'login';
+					target.innerText = 'Login';
+				});
+				return;
+			}
+
+			// Get the section that matches the anchor's custom attribute
+			const section = document.querySelector(`#${target.dataset.nav}`);
+			scrollToElement(section);
+		});
+
+		// Invoke scrollToElement when scrollBtn is clicked and send to the top of the HTML doc
+		scrollBtn.addEventListener('click', () => {
+			scrollToElement(document.documentElement);
+		});
+
+		// Sunco login
+		const loginUser = async (userData) => {
+			const { token } = await getToken(userData);
+			Smooch.login('m-scott', token);
+		};
+
+		const logoutUser = async () => {
+			Smooch.logout();
+		};
+
+		const getToken = async (userData) => {
+			const response = await fetch('http://localhost:3000/auth', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(userData),
+			});
+			if (!response.ok) {
+				window.alert(`HTTP error! status: ${response.status}`);
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			return await response.json();
+		};
+	},
+	false
+);
